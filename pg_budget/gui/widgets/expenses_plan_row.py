3b3563aec8b8c66aa14b1a200e2971943e67450f
Gael <prungael@gmail.com>
2025-10-07 08:30:50 +0200

    format date in table (fr)

--- pg_budget/gui/widgets/expenses_plan_row.py
+++ pg_budget/gui/widgets/expenses_plan_row.py
@@ -1,5 +1,6 @@
 """Custom Row for ExpensePlan"""
 
+from datetime import datetime
 from pg_budget.core.models.expenses_plan import ExpensesPlan
 from pg_budget.gui.widgets.base.base_row import BaseRow, RowField
 
@@ -8,11 +9,13 @@ class ExpensesPlanRow(BaseRow):
     """Class row for expense"""
 
     def __init__(self, expenses_plan: ExpensesPlan, parent=None):
+        start_date_obj = datetime.strptime(expenses_plan.start_date, "%Y-%m-%d")
+        end_date_obj = datetime.strptime(expenses_plan.end_date, "%Y-%m-%d")
         fields = [
             RowField("Name", value=expenses_plan.name),
             RowField("Amount", value=f"{expenses_plan.amount:.2f} €"),
-            RowField("Start Date", value=expenses_plan.start_date),
-            RowField("End Date", value=expenses_plan.end_date),
+            RowField("Start Date", value=start_date_obj.strftime("%d/%m/%Y")),
+            RowField("End Date", value=end_date_obj.strftime("%d/%m/%Y")),
         ]
 
         super().__init__(fields, expenses_plan.expensesplan_id, parent)
